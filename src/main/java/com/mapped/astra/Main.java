b4e7a847fd7a0afb43a30f97e7ef2fd54e4c0aaf
Kathiresan Selvaraj <kathiresan.selvaraj@datastax.com>
2022-01-14 20:33:12 -0500

    test conf update

--- src/main/java/com/mapped/astra/Main.java
+++ src/main/java/com/mapped/astra/Main.java
@@ -1,6 +1,7 @@
 package com.mapped.astra;
 
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.janusgraph.core.JanusGraph;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
+import java.util.Iterator;
 
 @SpringBootApplication(scanBasePackages = "com.mapped.astra")
 public class Main {
@@ -27,10 +29,15 @@ public class Main {
 
         LOGGER.info("Application is started");
 
-        GraphTraversalSource traversalSource = janusGraph.traversal();
-
-        traversalSource.V().hasNext();
-
+        GraphTraversalSource g = janusGraph.traversal();
+        janusGraph.addVertex().property("name","jim");
+        janusGraph.tx().commit();
+        //g.addV("employee").with("name","jim").next();
+        Iterator<Vertex> itr = g.V();
+        System.out.println("hasNext : " + itr.hasNext());
+        while(itr.hasNext()){
+          System.out.println(itr.next());
+        }
         LOGGER.info("The test case is finished");
 
     }
